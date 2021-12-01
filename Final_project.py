61413822da4355aa99fc64b88799a2a8694fb710
kmadhura2 <msk10@illinois.edu>
2021-12-01 00:26:07 -0600

    Population density added to the merged dataframe

--- Final_project.py
+++ Final_project.py
@@ -11,6 +11,7 @@ def aqi_data(file_name:str,top_five:pd.DataFrame, year:str)->None:
     aqi.rename(columns={'State': 'Location'}, inplace=True)
     aqi_skip=aqi.groupby(['Location','Year']).mean()
     merged_data = pd.merge(top_five, aqi_skip, on='Location', how="inner")
+    merged_data=merged_data.round(decimals=2)
     print("\n",year)
     print(merged_data)
     # smoking_data('Datasets/Smoking.csv', merged_data, column_name)
@@ -39,13 +40,16 @@ def aqi_data(file_name:str,top_five:pd.DataFrame, year:str)->None:
 #         remove = data_merge.drop(['Smoking-2016', 'Smoking-2017', 'Smoking-2018', 'Smoking-2019'], axis=1)
 #         print(remove)
 
-
 if __name__ == '__main__':
     population=pd.read_csv("Datasets/Population asthama.csv", skiprows=[1])
     smk_data = pd.read_csv('Datasets/Smoking.csv', skiprows=[1])
+    density = pd.read_csv("C:/Users/PC/Desktop/2021Fall_finals/Datasets/Land area.csv", usecols=['State', 'LandArea'])
+    density.rename(columns={'State': 'Location'}, inplace=True)
     data_merge = pd.merge(population, smk_data, on='Location', how='inner')
-    #print(data_merge)
-    population_year = pd.DataFrame(data_merge['Location'].copy())
+    population_year = pd.merge(population, density, on='Location', how="left")
+    population_year = population_year.drop(
+        columns=['Population-2016', 'Population-2017', 'Population-2018', 'Population-2019'], axis=1)
+    #population_year = pd.DataFrame(data_merge['Location'].copy())
     i=0
     for column_name, column_values in data_merge.items():
 
@@ -55,8 +59,11 @@ if __name__ == '__main__':
         if column_name=="Population-2016" and "Smoking-2016":
             population_year.insert(loc=1,column='Population',value=selection)
             population_year.insert(loc=2, column='Smoking',value=selection)
+            population_year.insert(loc=3, column='Population Density',
+                                   value=population_year['Population'] / population_year['LandArea'])
         else:
             population_year['Population']=selection
+            population_year['Population Density'] = population_year['Population'] / population_year['LandArea']
         population_year=population_year.sort_values(by=['Population'], ascending=False)
         files = ["Datasets/AQI/annual_aqi_by_county_2016.csv",
                  "Datasets/AQI/annual_aqi_by_county_2017.csv",
@@ -65,5 +72,4 @@ if __name__ == '__main__':
                  "Datasets/AQI/annual_aqi_by_county_2020.csv"]
         aqi_data(files[i], population_year.head(),column_name)
         population_year = population_year.sort_values(by=['Location'])
-
         i = i + 1
