a211818556dcc6fe184fe4bf8dc423ad8c43b13f
kmadhura2 <msk10@illinois.edu>
2021-12-02 21:26:18 -0600

    Annual Reports of 10 years added

--- Exploring_New_dataset.py
+++ Exploring_New_dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 def data(path):
     dataset=pd.read_csv(path)
+    dataset.rename(columns={'Edition': 'Year'}, inplace=True)
     list1=dataset['Measure Name'].unique()
     #print(list1)
     list2=['Access to Care - Annual','Asthma','Avoid Care Due to Cost','Illict Opioid Use',
@@ -16,9 +17,17 @@ def data(path):
     # print(df.head())
     df.drop(columns=['Report Type','Lower CI','Upper CI','Source','Source Year','Rank'], inplace=True)
     # df.groupby('Measure Name')
-    print(df)
+    print(df.head())
 
-data('Datasets/2020-Annual (1).csv')
+annual_filenames=['Datasets/Annual Reports/2011-Annual.csv','Datasets/Annual Reports/2012-Annual.csv',
+                  'Datasets/Annual Reports/2013-Annual.csv','Datasets/Annual Reports/2014-Annual.csv',
+                  'Datasets/Annual Reports/2015-Annual.csv','Datasets/Annual Reports/2016-Annual.csv',
+                  'Datasets/Annual Reports/2017-Annual.csv','Datasets/Annual Reports/2018-Annual.csv',
+                  'Datasets/Annual Reports/2019-Annual.csv','Datasets/Annual Reports/2020-Annual (1).csv']
+for i in range(len(annual_filenames)):
+    #function_name([i])
+    data(annual_filenames[i])
+#data('Datasets/2020-Annual (1).csv')
 
 
 
