68a01ac465d9dbce769f201b5a1b19eeff0779f5
kmadhura2 <msk10@illinois.edu>
2021-12-04 00:13:12 -0600

    Pivoted table merged with df dataframe.

--- Datasets_merging.py
+++ Datasets_merging.py
@@ -1,42 +1,44 @@
 import pandas as pd
 import numpy as np
 
-files = ["Datasets/population_2016.csv",
-         "Datasets/population_2017.csv",
-         "Datasets/population_2018.csv",
-         "Datasets/population_2019.csv"]
-filess =["Datasets/smoking_2016.csv",
-         "Datasets/smoking_2017.csv",
-         "Datasets/smoking_2018.csv",
-         "Datasets/smoking_2019.csv",]
-filesss=["Datasets/AQI/annual_aqi_by_county_2016.csv",
-         "Datasets/AQI/annual_aqi_by_county_2017.csv",
-         "Datasets/AQI/annual_aqi_by_county_2018.csv",
-         "Datasets/AQI/annual_aqi_by_county_2019.csv"]
-years=['2016','2017','2018','2019']
+population_files = ["Datasets/Population/population_2012.csv",
+                    "Datasets/Population/population_2013.csv","Datasets/Population/population_2014.csv",
+                    "Datasets/Population/population_2015.csv","Datasets/Population/population_2016.csv",
+                    "Datasets/Population/population_2017.csv","Datasets/Population/population_2018.csv",
+                    "Datasets/Population/population_2019.csv","Datasets/Population/population_2020.csv"]
+AQI_files=["Datasets/AQI/annual_aqi_by_county_2012.csv",
+           "Datasets/AQI/annual_aqi_by_county_2013.csv","Datasets/AQI/annual_aqi_by_county_2014.csv",
+           "Datasets/AQI/annual_aqi_by_county_2015.csv","Datasets/AQI/annual_aqi_by_county_2016.csv",
+           "Datasets/AQI/annual_aqi_by_county_2017.csv","Datasets/AQI/annual_aqi_by_county_2018.csv",
+           "Datasets/AQI/annual_aqi_by_county_2019.csv","Datasets/AQI/annual_aqi_by_county_2020.csv"]
+years=['2012','2013','2014','2015','2016','2017','2018','2019','2020']
 i = 0
-while i < len(files):
-    population = pd.read_csv(files[i], skiprows=[0,1],usecols=['Location','Total Residents'])
+while i < len(population_files):
+    population = pd.read_csv(population_files[i], skiprows=[0,1],usecols=['Location', 'Total Residents'])
     population=population.dropna()
     population.rename(columns={'Total Residents': 'Population'}, inplace=True)
-    smk_data = pd.read_csv(filess[i], skiprows=[0, 1], usecols=['Location', 'All Adults'])
-    smk_data = smk_data.dropna()
-    smk_data.rename(columns={'All Adults': 'Smoking'}, inplace=True)
-    data_merge = pd.merge(population, smk_data, on='Location', how='inner')
-    aqi = pd.read_csv(filesss[i], usecols=['State', 'County', 'Median AQI'])
+    #smk_data = pd.read_csv(filess[i], skiprows=[0, 1], usecols=['Location', 'All Adults'])
+    #smk_data = smk_data.dropna()
+    #smk_data.rename(columns={'All Adults': 'Smoking'}, inplace=True)
+    #data_merge = pd.merge(population, smk_data, on='Location', how='inner')
+    aqi = pd.read_csv(AQI_files[i], usecols=['State', 'County', 'Median AQI'])
     aqi.rename(columns={'State': 'Location'}, inplace=True)
     aqi_skip = aqi.groupby(['Location']).mean()
-    data_merge= pd.merge(data_merge,aqi_skip, on='Location', how='inner')
+    data_merge= pd.merge(population,aqi_skip, on='Location', how='inner')
     density = pd.read_csv("Datasets/Land area.csv", usecols=['State', 'LandArea'])
     density.rename(columns={'State': 'Location'}, inplace=True)
     data_merge = pd.merge(data_merge, density, on='Location', how='inner')
     data_merge.insert(loc=3, column='Population Density',
                            value=data_merge['Population'] / data_merge['LandArea'])
     data_merge.drop('LandArea',axis=1,inplace=True)
-
+    #data_merge['Location']=data_merge.index()
+    data_merge.set_index('Location', inplace=True)
     print('\n',years[i])
-    print(data_merge)
+    #print(data_merge)
     i=i+1
+
+
+
 
 # converting to csv
 data_merge.to_csv('Datasets/basic_info.csv')
--- Exploring_New_dataset.py
+++ Exploring_New_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-def data(path):
-    dataset=pd.read_csv(path)
+def data(annual_path, population_path, aqi_path,i):
+    dataset=pd.read_csv(annual_path)
     dataset.rename(columns={'Edition': 'Year'}, inplace=True)
     list1=dataset['Measure Name'].unique()
     #print(list1)
@@ -16,16 +16,66 @@ def data(path):
 
     # print(df.head())
     df.drop(columns=['Report Type','Lower CI','Upper CI','Source','Source Year','Rank'], inplace=True)
+    df.rename(columns={'State Name': 'Location'}, inplace=True)
 
-    df = df.pivot(index='State Name', columns='Measure Name', values='Value')
+    df = df.pivot(index='Location', columns='Measure Name', values='Value')
     # df.groupby('Measure Name')
-    print(df.head())
+    #print(df.head())
 
-annual_filenames=['Datasets/Annual Reports/2011-Annual.csv','Datasets/Annual Reports/2012-Annual.csv',
+    population = pd.read_csv(population_path, skiprows=[0, 1], usecols=['Location', 'Total Residents'])
+    population = population.dropna()
+    population.rename(columns={'Total Residents': 'Population'}, inplace=True)
+       # smk_data = pd.read_csv(filess[i], skiprows=[0, 1], usecols=['Location', 'All Adults'])
+        # smk_data = smk_data.dropna()
+        # smk_data.rename(columns={'All Adults': 'Smoking'}, inplace=True)
+        # data_merge = pd.merge(population, smk_data, on='Location', how='inner')
+    aqi = pd.read_csv(aqi_path, usecols=['State', 'County', 'Median AQI'])
+    aqi.rename(columns={'State': 'Location'}, inplace=True)
+    aqi_skip = aqi.groupby(['Location']).mean()
+    data_merge = pd.merge(population, aqi_skip, on='Location', how='inner')
+    density = pd.read_csv("Datasets/Land area.csv", usecols=['State', 'LandArea'])
+    density.rename(columns={'State': 'Location'}, inplace=True)
+    data_merge = pd.merge(data_merge, density, on='Location', how='inner')
+    data_merge.insert(loc=3, column='Population Density',
+                          value=data_merge['Population'] / data_merge['LandArea'])
+    data_merge.drop('LandArea', axis=1, inplace=True)
+        # data_merge['Location']=data_merge.index()
+    data_merge.set_index('Location', inplace=True)
+    #print('\n', years[i])
+    #data_merge=pd.merge(data_merge,df, how='inner')
+    frames=[df,data_merge]
+    data_merge=pd.concat(frames,axis=1, join="inner")
+    frames_1 = [final, data_merge]
+    result = pd.concat(frames_1, axis=0, join="inner")
+    ##data_merge=pd.concat(frames_1,axis=0)
+    print(result)
+    #i = i + 1
+    #data_merge.append(year)
+    #print(data_merge)
+    #frames_1=[final,data_merge]
+    #final_1=pd.concat(frames_1, axis=0)
+    #print(final_1)
+    return data_merge
+
+annual_filenames=['Datasets/Annual Reports/2012-Annual.csv',
                   'Datasets/Annual Reports/2013-Annual.csv','Datasets/Annual Reports/2014-Annual.csv',
                   'Datasets/Annual Reports/2015-Annual.csv','Datasets/Annual Reports/2016-Annual.csv',
                   'Datasets/Annual Reports/2017-Annual.csv','Datasets/Annual Reports/2018-Annual.csv',
                   'Datasets/Annual Reports/2019-Annual.csv','Datasets/Annual Reports/2020-Annual (1).csv']
+population_files = ["Datasets/Population/population_2012.csv",
+                    "Datasets/Population/population_2013.csv","Datasets/Population/population_2014.csv",
+                    "Datasets/Population/population_2015.csv","Datasets/Population/population_2016.csv",
+                    "Datasets/Population/population_2017.csv","Datasets/Population/population_2018.csv",
+                    "Datasets/Population/population_2019.csv","Datasets/Population/population_2020.csv"]
+AQI_files=["Datasets/AQI/annual_aqi_by_county_2012.csv",
+           "Datasets/AQI/annual_aqi_by_county_2013.csv","Datasets/AQI/annual_aqi_by_county_2014.csv",
+           "Datasets/AQI/annual_aqi_by_county_2015.csv","Datasets/AQI/annual_aqi_by_county_2016.csv",
+           "Datasets/AQI/annual_aqi_by_county_2017.csv","Datasets/AQI/annual_aqi_by_county_2018.csv",
+           "Datasets/AQI/annual_aqi_by_county_2019.csv","Datasets/AQI/annual_aqi_by_county_2020.csv"]
+years=['2012','2013','2014','2015','2016','2017','2018','2019','2020']
+final=pd.DataFrame
 for i in range(len(annual_filenames)):
     #function_name([i])
-    data(annual_filenames[i])
+    result= data(annual_filenames[i],population_files[i],AQI_files[i],i)
+   #final= pd.concat([final,data(annual_filenames[i],population_files[i],AQI_files[i],i)],axis=0)
+    #final=final.append(other=result)
