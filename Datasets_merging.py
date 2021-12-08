30fcd1ef577049648781ad7d492907cf1107e12d
Himank00 <himankdave26@gmail.com>
2021-12-07 22:30:25 -0600

    Code cleaning

--- Datasets_merging.py
+++ Datasets_merging.py
@@ -1,34 +1,28 @@
 import pandas as pd
+import numpy as np
+from scipy.stats import skew
+import matplotlib.pyplot as plt
 
-def data(annual_path, population_path, aqi_path,i):
+def data(annual_path, population_path, aqi_path,i) -> pd.DataFrame():
     dataset=pd.read_csv(annual_path)
     dataset.rename(columns={'Edition': 'Year'}, inplace=True)
     list1=dataset['Measure Name'].unique()
     #print(list1)
-    list2=['Access to Care - Annual','Asthma','Avoid Care Due to Cost','Illicit Opioid Use',
-           'Crowded Housing', 'Smoking']
+    list2=['Asthma','Smoking','Air Pollution']
     df=pd.DataFrame()
     for i in range(len(list1)):
         if list1[i] in list2:
-            #print(list1[i])
             df=df.append(dataset.loc[(dataset['Measure Name']== list1[i])])
-            #dataset.drop(list1[i],axis=1)
 
-    # print(df.head())
+
     df.drop(columns=['Report Type','Lower CI','Upper CI','Source','Source Year','Rank'], inplace=True)
     df.rename(columns={'State Name': 'Location'}, inplace=True)
 
     df = df.pivot(index='Location', columns='Measure Name', values='Value')
-    # df.groupby('Measure Name')
-    #print(df.head())
 
     population = pd.read_csv(population_path, skiprows=[0, 1], usecols=['Location', 'Total Residents'])
     population = population.dropna()
     population.rename(columns={'Total Residents': 'Population'}, inplace=True)
-       # smk_data = pd.read_csv(filess[i], skiprows=[0, 1], usecols=['Location', 'All Adults'])
-        # smk_data = smk_data.dropna()
-        # smk_data.rename(columns={'All Adults': 'Smoking'}, inplace=True)
-        # data_merge = pd.merge(population, smk_data, on='Location', how='inner')
     aqi = pd.read_csv(aqi_path, usecols=['State', 'County', 'Median AQI'])
     aqi.rename(columns={'State': 'Location'}, inplace=True)
     aqi_skip = aqi.groupby(['Location']).mean()
@@ -36,30 +30,39 @@ def data(annual_path, population_path, aqi_path,i):
     density = pd.read_csv("Datasets/Land area.csv", usecols=['State', 'LandArea'])
     density.rename(columns={'State': 'Location'}, inplace=True)
     data_merge = pd.merge(data_merge, density, on='Location', how='inner')
-    data_merge.insert(loc=3, column='Population Density',
+    data_merge.insert(loc=3, column='Population_Density',
                           value=data_merge['Population'] / data_merge['LandArea'])
     data_merge.drop('LandArea', axis=1, inplace=True)
-        # data_merge['Location']=data_merge.index()
+
     data_merge.set_index('Location', inplace=True)
-    #print('\n', years[i])
-    #data_merge=pd.merge(data_merge,df, how='inner')
+
     frames=[df,data_merge]
     data_merge=pd.concat(frames,axis=1, join="inner")
-    #result = pd.concat(frames_1, axis=0, join="inner")
-    ##data_merge=pd.concat(frames_1,axis=0)
-    #print(result)
-    #i = i + 1
-    #data_merge.append(year)
-    print(data_merge.head())
-    #frames_1=[final,data_merge]
-    #final_1=pd.concat(frames_1, axis=0)
-    #print(final_1)
 
-    print('\n',years[i])
-    print(data_merge)
-    data_append=data_append.append(data_merge)
-    i=i+1
+    return data_merge
 
-# converting to csv
-print(data_append)
-data_append.to_csv('Datasets/basic_info.csv')
+
+if __name__ == '__main__':
+    annual_filenames=['Datasets/Annual Reports/2012-Annual.csv',
+                      'Datasets/Annual Reports/2013-Annual.csv','Datasets/Annual Reports/2014-Annual.csv',
+                      'Datasets/Annual Reports/2015-Annual.csv','Datasets/Annual Reports/2016-Annual.csv',
+                      'Datasets/Annual Reports/2017-Annual.csv','Datasets/Annual Reports/2018-Annual.csv',
+                      'Datasets/Annual Reports/2019-Annual.csv','Datasets/Annual Reports/2020-Annual (1).csv']
+    population_files = ["Datasets/Population/population_2012.csv",
+                        "Datasets/Population/population_2013.csv","Datasets/Population/population_2014.csv",
+                        "Datasets/Population/population_2015.csv","Datasets/Population/population_2016.csv",
+                        "Datasets/Population/population_2017.csv","Datasets/Population/population_2018.csv",
+                        "Datasets/Population/population_2019.csv","Datasets/Population/population_2020.csv"]
+    AQI_files=["Datasets/AQI/annual_aqi_by_county_2012.csv",
+               "Datasets/AQI/annual_aqi_by_county_2013.csv","Datasets/AQI/annual_aqi_by_county_2014.csv",
+               "Datasets/AQI/annual_aqi_by_county_2015.csv","Datasets/AQI/annual_aqi_by_county_2016.csv",
+               "Datasets/AQI/annual_aqi_by_county_2017.csv","Datasets/AQI/annual_aqi_by_county_2018.csv",
+               "Datasets/AQI/annual_aqi_by_county_2019.csv","Datasets/AQI/annual_aqi_by_county_2020.csv"]
+    years=['2012','2013','2014','2015','2016','2017','2018','2019','2020']
+    final=pd.DataFrame()
+    temp=pd.DataFrame()
+    for i in range(len(years)):
+        temp=data(annual_filenames[i],population_files[i],AQI_files[i],i)
+        final=final.append(temp)
+
+    final.to_csv('Datasets/DATA.csv')
--- Exploring_New_dataset.py
+++ Exploring_New_dataset.py
@@ -2,61 +2,8 @@ import pandas as pd
 import numpy as np
 from scipy.stats import skew
 import matplotlib.pyplot as plt
-#from matplotlib import pyplot as plt
-def data(annual_path, population_path, aqi_path,i):
-    dataset=pd.read_csv(annual_path)
-    dataset.rename(columns={'Edition': 'Year'}, inplace=True)
-    list1=dataset['Measure Name'].unique()
-    #print(list1)
-    list2=['Access to Care - Annual','Asthma','Avoid Care Due to Cost','Illict Opioid Use',
-           'Crowded Housing', 'Smoking']
-    df=pd.DataFrame()
-    for i in range(len(list1)):
-        if list1[i] in list2:
-            #print(list1[i])
-            df=df.append(dataset.loc[(dataset['Measure Name']== list1[i])])
-            #dataset.drop(list1[i],axis=1)
 
-    # print(df.head())
-    df.drop(columns=['Report Type','Lower CI','Upper CI','Source','Source Year','Rank'], inplace=True)
-    df.rename(columns={'State Name': 'Location'}, inplace=True)
-
-    df = df.pivot(index='Location', columns='Measure Name', values='Value')
-    # df.groupby('Measure Name')
-    #print(df.head())
-
-    population = pd.read_csv(population_path, skiprows=[0, 1], usecols=['Location', 'Total Residents'])
-    population = population.dropna()
-    population.rename(columns={'Total Residents': 'Population'}, inplace=True)
-       # smk_data = pd.read_csv(filess[i], skiprows=[0, 1], usecols=['Location', 'All Adults'])
-        # smk_data = smk_data.dropna()
-        # smk_data.rename(columns={'All Adults': 'Smoking'}, inplace=True)
-        # data_merge = pd.merge(population, smk_data, on='Location', how='inner')
-    aqi = pd.read_csv(aqi_path, usecols=['State', 'County', 'Median AQI'])
-    aqi.rename(columns={'State': 'Location'}, inplace=True)
-    aqi_skip = aqi.groupby(['Location']).mean()
-    data_merge = pd.merge(population, aqi_skip, on='Location', how='inner')
-    density = pd.read_csv("Datasets/Land area.csv", usecols=['State', 'LandArea'])
-    density.rename(columns={'State': 'Location'}, inplace=True)
-    data_merge = pd.merge(data_merge, density, on='Location', how='inner')
-    data_merge.insert(loc=3, column='Population Density',
-                          value=data_merge['Population'] / data_merge['LandArea'])
-    data_merge.drop('LandArea', axis=1, inplace=True)
-        # data_merge['Location']=data_merge.index()
-    data_merge.set_index('Location', inplace=True)
-    #print('\n', years[i])
-    #data_merge=pd.merge(data_merge,df, how='inner')
-    frames=[df,data_merge]
-    data_merge=pd.concat(frames,axis=1, join="inner")
-    #result = pd.concat(frames_1, axis=0, join="inner")
-    ##data_merge=pd.concat(frames_1,axis=0)
-    #print(result)
-    #i = i + 1
-    #data_merge.append(year)
-    #print(data_merge.head())
-    #frames_1=[final,data_merge]
-    #final_1=pd.concat(frames_1, axis=0)
-    #print(final_1)
+def data(data_merge):
     for location in data_merge.index:
         data_merge['Asthma'][location] = (data_merge['Asthma'][location] * data_merge['Population'][location]) / 100
     data_merge.insert(5, 'Metric', '0.0')
@@ -64,17 +11,12 @@ def data(annual_path, population_path, aqi_path,i):
     quantile_50=data_merge.quantile(0.5, axis = 0)
     quantile_100 = data_merge.quantile(1.0, axis=0)
     quantile_75=data_merge.quantile(0.75, axis=0)
-    #print(skewed_data)
     print(quantile_50)
     print(quantile_75)
     print(quantile_100)
     data_merge['Metric'] = data_merge['Metric'].astype(float)
     for i, row in data_merge.iterrows():
-        #print(row[0],[row[2]])
-        #row[0]=(row[0]*row[2])/100
         sum=0
-        #print(f"Index: {i}")
-        #print(f"{row[0]}\n")
         if (row[2] >= quantile_75[2] and row[4] >= quantile_75[4] and row[0] >= quantile_75[0]) or \
                 (quantile_50[2]<=row[2]<quantile_75[2] and quantile_50[4]<=row[4]<quantile_75[4] and
                  quantile_50[0]<=row[0]<quantile_75[0]) or (row[2]<quantile_50[2] and row[4]<quantile_50[4]and row[0]<quantile_50[0]):
@@ -120,42 +62,17 @@ def data(annual_path, population_path, aqi_path,i):
 
 
         row[5]=sum
-        #print(row[5])
-        #plt.plot(row[0],row[2])
-        #plt.show()
-    #print(data_merge['Metric'])
     print(data_merge.sort_values(by=['Population'], inplace=False))
-    #print('\n')
-    print(data_merge[['Population','Asthma', 'Population Density']])
+    print(data_merge[['Population','Asthma', 'Population_Density']])
     plt.figure(figsize=(8,5))
-    plt.plot(data_merge.index, data_merge.Population,'red',label="Population")
     plt.plot(data_merge.index, data_merge.Asthma, 'blue', label="Asthma")
+    plt.plot(data_merge.index, data_merge.Population_Density,'green', label="Population_Density")
     plt.title('Population VS Asthma')
     plt.ylabel('Number of persons')
     plt.legend()
     plt.show()
-    #print(data_merge.index)
 
+if __name__ == '__main__':
+    df=pd.read_csv('Datasets/DATA.csv')
+    data(df)
 
-annual_filenames=['Datasets/Annual Reports/2012-Annual.csv',
-                  'Datasets/Annual Reports/2013-Annual.csv','Datasets/Annual Reports/2014-Annual.csv',
-                  'Datasets/Annual Reports/2015-Annual.csv','Datasets/Annual Reports/2016-Annual.csv',
-                  'Datasets/Annual Reports/2017-Annual.csv','Datasets/Annual Reports/2018-Annual.csv',
-                  'Datasets/Annual Reports/2019-Annual.csv','Datasets/Annual Reports/2020-Annual (1).csv']
-population_files = ["Datasets/Population/population_2012.csv",
-                    "Datasets/Population/population_2013.csv","Datasets/Population/population_2014.csv",
-                    "Datasets/Population/population_2015.csv","Datasets/Population/population_2016.csv",
-                    "Datasets/Population/population_2017.csv","Datasets/Population/population_2018.csv",
-                    "Datasets/Population/population_2019.csv","Datasets/Population/population_2020.csv"]
-AQI_files=["Datasets/AQI/annual_aqi_by_county_2012.csv",
-           "Datasets/AQI/annual_aqi_by_county_2013.csv","Datasets/AQI/annual_aqi_by_county_2014.csv",
-           "Datasets/AQI/annual_aqi_by_county_2015.csv","Datasets/AQI/annual_aqi_by_county_2016.csv",
-           "Datasets/AQI/annual_aqi_by_county_2017.csv","Datasets/AQI/annual_aqi_by_county_2018.csv",
-           "Datasets/AQI/annual_aqi_by_county_2019.csv","Datasets/AQI/annual_aqi_by_county_2020.csv"]
-years=['2012','2013','2014','2015','2016','2017','2018','2019','2020']
-final=pd.DataFrame
-for i in range(1):
-    #function_name([i])
-    data(annual_filenames[i],population_files[i],AQI_files[i],i)
-   #final= pd.concat([final,data(annual_filenames[i],population_files[i],AQI_files[i],i)],axis=0)
-    #final=final.append(other=result)
